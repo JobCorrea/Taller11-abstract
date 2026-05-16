471ba8cb71972545d993e2ebf85920e233ad6cce
Job Daniel Correa <jobcorre0912@gmail.com>
2026-05-15 22:57:51 -0500

    Intaciazion de objetos de clase Empleado-Genrente-Vendedor

--- Main.java
+++ Main.java
@@ -6,6 +6,11 @@ public class Main {
         rectangulo.mostrarArea();
         System.out.println("---------------------");
 
-        
+        Empleado gerente = new Gerente("Isabel", 1200000);
+        Empleado vendedor = new Vendedor("Job", 1000000, 5);
+        gerente.mostrarDetalles();
+        vendedor.mostrarDetalles();
+
+           
     }
 }
