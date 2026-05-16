1673bf48a20f981d413cc9634736712150738256
Job Daniel Correa <jobcorre0912@gmail.com>
2026-05-15 22:55:34 -0500

    Intaciazion de objetos de clase Figura-Circulo-Rectangulo

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        Figura circulo = new Circulo(5);
+        circulo.mostrarArea();
+        Figura rectangulo = new Rectangulo(8, 4);
+        rectangulo.mostrarArea();
+        System.out.println("---------------------");
+
         
     }
 }
